364907d38c75d6e87c722cf890a33dbeb8ae4f3a
Michael Schumann <schumann82@gmail.com>
2018-07-22 10:40:27 -0700

    display products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,5 +18,14 @@ var connection = mysql.createConnection({
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    
+    prodDisplay();
   });
+
+  function prodDisplay() {
+    connection.query("SELECT id, product_name, price FROM products", function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+          console.log(res[i].id + " " + res[i].product_name + " " + res[i].price);
+        }
+        console.log("-----------------------------------");
+      });
+  }

c0d0ffe17d2dac80014f753413523472aae2b34b
Michael Schumann <schumann82@gmail.com>
2018-07-22 13:02:02 -0700

    subtracts orders from inventory

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,9 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
+let productID;
+let prodQuantity;
+
 var connection = mysql.createConnection({
     host: "localhost",
   
@@ -26,6 +29,52 @@ var connection = mysql.createConnection({
         for (var i = 0; i < res.length; i++) {
           console.log(res[i].id + " " + res[i].product_name + " " + res[i].price);
         }
-        console.log("-----------------------------------");
+        console.log("-----------------------------------\n");
+        console.log("Hit any key to continue.\n");
       });
+      whatchaWant();
+  }
+
+  function whatchaWant() {
+    inquirer
+    .prompt([
+      // Here we create a basic text prompt.
+      {
+        type: "input",
+        message: "What is id of the item you wish to purchase?\n",
+        name: "id"
+      },
+      {
+      type: "input",
+      message: "How many units would you like to buy?\n",
+      name: "quantity"
+    },
+     
+    ])
+      .then(function(inquirerResponse) {
+        productID = inquirerResponse.id;
+        prodQuantity = inquirerResponse.quantity;
+
+        bought(productID, prodQuantity);
+      
+      });
+  }
+
+  function bought(productID, prodQuantity) {
+    var query = connection.query(
+        "UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ?",
+        [
+            
+              prodQuantity
+            ,
+            
+              productID
+            
+          ],
+        function(err, res) {
+            
+          console.log(res + " products updated!\n");
+         
+        }
+      );
   }
